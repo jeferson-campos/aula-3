2daf9d112521b5fa2c0f6c20992914043b752b46
jeferson-campos <jefersoncampos5296@gmail.com>
2024-10-14 12:39:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+// let soma = 0;
+// let hello;
+
+// do {
+//     hello = prompt("Digite um número (ou insira qualquer coisa não numérica para parar):");
+//     if (!isNaN(parseFloat(hello))) {
+//         soma += parseFloat(hello); 
+//     }
+// } while (!isNaN(parseFloat(hello)));
+// console.log("A soma dos números inseridos é: " + soma);
+// Define o número a ser adivinhado
+// const numeroCorreto = 7;
+// let chute;
+
+// do {
+//     chute = parseInt(prompt("Adivinhe um número entre 1 e 10:"));
+
+//     if (chute !== numeroCorreto) {
+//         console.log("Tente novamente!");
+//     }
+
+// } while (chute !== numeroCorreto);
+
+// console.log("Parabéns! Você adivinhou o número correto: " + numeroCorreto);
+// let numero = parseInt(prompt("Digite um número inteiro positivo:"));
+
+// if (numero > 0) {
+//     let soma = 0;
+//     let contador = 1;
+
+//     while (contador <= numero) {
+//         soma += contador;
+//         contador++;
+//     }
+
+//     console.log("A soma de todos os números inteiros de 1 até " + numero + " é: " + soma);
+// } else {
+//     console.log("Por favor, insira um número inteiro positivo.");
+// }
